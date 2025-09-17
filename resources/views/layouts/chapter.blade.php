b67a634703f82825944cecfed5a1476c6be21d77
LouCyanCoder <lucian.n@seznam.cz>
2025-09-17 21:27:59 +0200

    reran the project and changed some styles

--- resources/views/layouts/chapter.blade.php
+++ resources/views/layouts/chapter.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.main')
 
 @section('content')
-<div class='chapter_container'>
-    @if($chapter->is_end=='1')
-        <h2>Game Over</h2>
-        <a href='/show/1'>Restart game</a>
-    @else
+    <div class='chapter container'>
+        @if ($chapter->is_end == '1')
+            <h2>Game Over</h2>
+            <a href='/show/1'>Restart game</a>
+        @else
+            @if (isset($illustration->filename))
+                <img src="{{ asset('img/' . $illustration->filename) }}" alt="illustration">
+            @endif
 
-        @if(isset($illustration->filename))
-            <img src="{{asset('img/'.$illustration->filename)}}" alt="illustration">
-        @endif
-
-        <p>
+            <div class="chapter__text">
                 <?= htmlspecialchars_decode($chapter->text) ?>
-        </p>
 
-        <div class='choices'>
-        @foreach ($choices as $choice)
-            <a href="./{{ $choice->goto_id }}">
-                <button type='button' class='btn btn-sm'>
-                    {{ $choice->text }}
-                </button>
-            </a>
-
-                @endforeach
             </div>
-    @endif
-</div>
 
-    @endsection
+
+            <div class='choices'>
+
+                @foreach ($choices as $choice)
+                    <a href="./{{ $choice->goto_id }}">
+                        <button type='button' class='btn btn-sm'>
+                            {{ $choice->text }}
+                        </button>
+                    </a>
+                @endforeach
+
+            </div>
+        @endif
+    </div>
+
+@endsection
